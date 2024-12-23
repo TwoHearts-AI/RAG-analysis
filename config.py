c4689c92d9a407bdedd8761339bdbfd0d6b2e10a
alimc <Alimchik.228@mail.ru>
2024-12-23 15:41:08 +0300

    update reranker

--- config.py
+++ config.py
@@ -8,9 +8,11 @@ class Settings(BaseSettings):
     DEBUG: bool = Field(False, description="Debug mode for FastAPI")
 
     # Qdrant Configuration
-    QDRANT_URL: str = Field("http://188.227.32.69:6333", description="Qdrant server")
+    QDRANT_URL: str = Field("qdrant:6333", description="Qdrant server")
 
-    MISTRAL_API_KEY: str = Field('DQi1WtQ4eFNStpr0aZyZLKa71V0TmZhk', description="Mistral API key")
+
+    MISTRAL_API_KEY: str = Field(..., description="Mistral API key")
+
     MISTRAL_MODEL: str = Field(
         "mistral-large-latest",
         description="Mistral model name to use"
